97dfb774a3e4a4c9957d9dc578e7f5a4120b3fce
alexis Montealegre <ajmu26@gmail.com>
2020-07-02 20:38:42 -0400

    done

--- patrones.rb
+++ patrones.rb
@@ -54,7 +54,7 @@ zed.times do
 end
 print "\n"
 
-###################
+#####################
 
 num = ARGV[0].to_i
 (num).times do |i|
@@ -67,3 +67,67 @@ num = ARGV[0].to_i
     end
     print "\n"
 end
+
+#####################
+
+num = ARGV[0].to_i
+num.times do |i|
+    print "*"
+end
+print"\n"
+
+(num - 2).times do |i|
+    print "*"
+    (num - 2).times do |j|
+    if i!=j
+        print " "
+    else
+        print "*"
+    end
+end
+    print "*"
+    print "\n"
+end
+
+num.times do |i|
+    print "*"
+end
+print"\n"
+
+#####################
+
+num = ARGV[0].to_i
+body = num / 2
+num - 4.times do |i|
+    puts " "
+    space = ""
+    num.times do |j|
+        if j < body - i-1
+            space << " "
+        elsif j >= body - i && j <= body + i
+            space << "*"
+        else
+            space << " " 
+        end
+    end
+    puts space
+end
+(num - 3).times do |i|
+    (num - 2).times do |i|
+        if i == num / 2
+            print "*"
+        else
+            print " "
+        end
+    end
+    print "\n"
+    end
+
+(num).times do |i|
+    if  i == 0 || i == num - 1
+        print " "
+    else
+        print "*"
+    end
+end
+print "\n"
